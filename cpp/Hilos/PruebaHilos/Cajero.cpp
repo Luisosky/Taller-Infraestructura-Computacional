d22fd22ad04ec1a7708291a10d13cba57a61fbf8
Luis Carlos Calderón Calvo <128423397+Luisosky@users.noreply.github.com>
2024-10-07 21:23:57 -0500

    Mejora al hilos cpp
    Se mejora el uso de hilos en cpp

--- cpp/Hilos/PruebaHilos/Cajero.cpp
+++ cpp/Hilos/PruebaHilos/Cajero.cpp
@@ -6,30 +6,30 @@
 
 Cajero::Cajero(const std::string &nombre) {}
 
-void Cajero::procesarCompra(const Cliente& cliente, const std::chrono::time_point<std::chrono::system_clock>& initialTime) {
-            std::cout << "La cajera " << nombre << " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
-                      << cliente.getNombre() << " EN EL TIEMPO: "
-                      << obtenerSegundos(initialTime) << "seg\n";
+    void Cajero::procesarCompra(const Cliente& cliente, const std::chrono::time_point<std::chrono::system_clock>& initialTime) {
+        std::cout << "La cajera " << nombre << " COMIENZA A PROCESAR LA COMPRA DEL CLIENTE "
+                  << cliente.getNombre() << " EN EL TIEMPO: "
+                  << obtenerSegundos(initialTime) << "seg\n";
 
-            const std::vector<int>& productos = cliente.getProductos();
-            for (size_t i = 0; i < productos.size(); ++i) {
-                esperarXsegundos(productos[i]);
-                std::cout << "Procesado el producto " << (i + 1) << " -> Tiempo: "
-                          << obtenerSegundos(initialTime) << "seg\n";
-            }
-
-            std::cout << "La cajera " << nombre << " HA TERMINADO DE PROCESAR "
-                      << cliente.getNombre() << " EN EL TIEMPO: "
+        const std::vector<int>& productos = cliente.getProductos();
+        for (size_t i = 0; i < productos.size(); ++i) {
+            esperarXsegundos(productos[i]);
+            std::cout << "Procesado el producto " << (i + 1) << " -> Tiempo: "
                       << obtenerSegundos(initialTime) << "seg\n";
         }
 
-        void Cajero::esperarXsegundos(int segundos) {
-            std::this_thread::sleep_for(std::chrono::seconds(segundos));
-        }
+        std::cout << "La cajera " << nombre << " HA TERMINADO DE PROCESAR "
+                  << cliente.getNombre() << " EN EL TIEMPO: "
+                  << obtenerSegundos(initialTime) << "seg\n";
+    }
+
+    void Cajero::esperarXsegundos(int segundos) {
+        std::this_thread::sleep_for(std::chrono::seconds(segundos));
+    }
 
 
-        long Cajero::obtenerSegundos(const std::chrono::time_point<std::chrono::system_clock>& initialTime) {
-            auto tiempoActual = std::chrono::system_clock::now();
-            auto duracion = std::chrono::duration_cast<std::chrono::seconds>(tiempoActual - initialTime);
-            return duracion.count();
-        }
+    long Cajero::obtenerSegundos(const std::chrono::time_point<std::chrono::system_clock>& initialTime) {
+        auto tiempoActual = std::chrono::system_clock::now();
+        auto duracion = std::chrono::duration_cast<std::chrono::seconds>(tiempoActual - initialTime);
+        return duracion.count();
+    }
--- cpp/Hilos/PruebaHilos/main.cpp
+++ cpp/Hilos/PruebaHilos/main.cpp
@@ -3,17 +3,21 @@
 #include "Cajero.h"
 
 int main() {
+    // Tiempo inicial de referencia
+    auto initialTime = std::chrono::system_clock::now();
+
     Cliente cliente1("Cliente 1", {2, 2, 1, 5, 2, 3});
     Cliente cliente2("Cliente 2", {1, 3, 5, 1, 1});
 
     Cajero cajera1("Cajera 1");
     Cajero cajera2("Cajera 2");
 
-    // Tiempo inicial de referencia
-    auto initialTime = std::chrono::system_clock::now();
 
-    cajera1.procesarCompra(cliente1, initialTime);
-    cajera2.procesarCompra(cliente2, initialTime);
+    std::thread t1(&Cajero::procesarCompra, &cajera1, std::ref(cliente1), initialTime);
+    std::thread t2(&Cajero::procesarCompra, &cajera2, std::ref(cliente2), initialTime);
+
+    t1.join();
+    t2.join();
 
     return 0;
 }
